d9abbff3746a70d02065bcc5b14b9961aec00832
Zhipeng Jia <zhipeng.jia@outlook.com>
2015-04-21 17:10:02 +0800

    Check empty content

--- cloud/validators.js
+++ cloud/validators.js
@@ -1,9 +1,14 @@
 'use strict';
 
+var _ = require('lodash');
+
 exports.Hole = function (req, res) {
   var hole = req.object;
   if (hole.get('author').get('objectId') !== req.user.get('objectId')) {
     return res.error('Cannot post hole as another user!');
+  }
+  if (_.isEmpty(hole.get('content'))) {
+    return res.error('Empty content!');
   }
   res.success();
 };
@@ -12,6 +17,9 @@ exports.Comment = function (req, res) {
   var comment = req.object;
   if (comment.get('author').get('objectId') !== req.user.get('objectId')) {
     return res.error('Cannot post comment as another user!');
+  }
+  if (_.isEmpty(comment.get('content'))) {
+    return res.error('Empty content!');
   }
   res.success();
 };
