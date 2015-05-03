89af2ea736f44be47921967f3c573b00ec1fb457
Zhipeng Jia <zhipeng.jia@outlook.com>
2015-05-03 19:06:26 +0800

    Push new notification

--- cloud/notifications.js
+++ cloud/notifications.js
@@ -6,13 +6,16 @@ var Notification = require('cloud/models').Notification;
 exports.Comment = function (req) {
   var comment = req.object;
   var hole;
+  var toUser;
+  var promise;
   if (comment.get('replyTo')) {
-    return comment.get('replyTo').fetch().then(function (targetComment) {
+    promise = comment.get('replyTo').fetch().then(function (targetComment) {
       return targetComment.get('realAuthor').fetch();
     }).then(function (author) {
       if (author.getObjectId() === comment.get('realAuthor').getObjectId()) {
         return AV.Promise.as();
       }
+      toUser = author;
       var acl = new AV.ACL();
       acl.setReadAccess(author, true);
       acl.setWriteAccess(author, true);
@@ -25,13 +28,14 @@ exports.Comment = function (req) {
       return notification.save();
     });
   } else {
-    return comment.get('hole').fetch().then(function (result) {
+    promise = comment.get('hole').fetch().then(function (result) {
       hole = result;
       return hole.get('realAuthor').fetch();
     }).then(function (author) {
       if (author.getObjectId() === comment.get('realAuthor').getObjectId()) {
         return AV.Promise.as();
       }
+      toUser = author;
       var acl = new AV.ACL();
       acl.setReadAccess(author, true);
       acl.setWriteAccess(author, true);
@@ -44,4 +48,14 @@ exports.Comment = function (req) {
       return notification.save();
     });
   }
+  return promise.then(function () {
+    if (toUser) {
+      return AV.Push.send({
+        channels: ['notification_' + toUser.getObjectId()],
+        data: { action: 'checkNotification' }
+      });
+    } else {
+      return AV.Promise.as(); 
+    }
+  });
 };
